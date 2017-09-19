c358bb4168fabc3f1f2eeef9839ddf6ba2d26f43
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-09-19 12:07:48 +0200

    første udkast sql

---
+++ Recipes/src/main/java/Connector.java
@@ -0,0 +1,35 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author Benedikte
+ * This DBConnector should connect to my remote server and a database called smallsteps
+ * 
+ */
+@WebServlet(name = "RecipesServlet", urlPatterns = {"/recipesServlet"})
+
+public class Connector {
+    
+    
+    
+    private final static String DRIVER = "com.mysql.jdbc.Driver";
+    private final static String URL = "jdbc:mysql://46.101.179.58:3306/test";
+    private final static String USER = "testuser";
+    private final static String PASSWORD = "password123";
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null ;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+}
---
+++ Recipes/src/main/webapp/addRecipe.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : addRecipe
+    Created on : 19-09-2017, 08:34:36
+    Author     : Ejer
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
